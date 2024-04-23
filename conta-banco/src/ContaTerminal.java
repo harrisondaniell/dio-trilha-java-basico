5fbd685979b990c205e563fcee6f15728100c9c3
harrison <d.harrison.lima@gmail.com>
2024-04-23 17:49:18 -0300

    Projeto conta bancária

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o numero da sua conta: ");
+        int numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Digite o numero da sua agência: ");
+        String agencia = sc.next();
+        sc.nextLine();
+
+        System.out.println("Digite o seu nome completo: ");
+        String nome = sc.nextLine();
+
+
+        System.out.println("Digite o saldo atual da sua conta: ");
+        double saldo = sc.nextDouble();
+
+        System.out.printf("Numero da conta: %d %nNumero da agência: %s %nNome do titular: %s%nSaldo atual: %.2f",
+                numero, agencia, nome, saldo);
+
+    }
+}
